2e757c2f74a92ea0b2ac7df814899cbf89883831
melanopsin <melanopsin@melanopsin.psych.upenn.edu>
2017-11-08 12:17:13 -0500

    Updated host name

--- code/protocols/PulseSquint/RunPulseSquintTrials.m
+++ code/protocols/PulseSquint/RunPulseSquintTrials.m
@@ -32,7 +32,7 @@ protocolParams.simulate.makePlots = true;
 % define the identities of the base computer (which oversees the
 % experiment and controls the OneLight) and the satellite computers that
 % handle EMG and pupil recording
-protocolParams.hostNames = {'gka06', 'monkfish', 'gka04'};
+protocolParams.hostNames = {'gka06', 'monkfish', 'melanopsin'};
 protocolParams.hostIPs = {'128.91.59.227', '128.91.59.157', '128.91.59.222'};
 protocolParams.hostRoles = {'base', 'satellite', 'satellite'};
 protocolParams.hostActions = {{'operator','observer','oneLight'}, 'pupil', 'emg'};
