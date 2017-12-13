78ae7624d87ed33af5b20c51a4741d5e770b604e
melanopsin <melanopsin@monkfish.psych.upenn.edu>
2017-12-13 16:16:52 -0500

    setup IR camera now functional
    upon running RunPulseSquintTrials, after validation before the experiment, the pupil satellite computer (monkfish) opens and displays the output of the IR camera
    -the idea is during this time period, we can adjust the rig to get the pupil into the proper position
    -once everything is set, the operator pushes enter on the pupil satellite computer to advance
    -note that we’re not actually simulating a trial here, we’re just opening and displaying the camera

--- code/protocols/PulseSquint/RunPulseSquintTrials.m
+++ code/protocols/PulseSquint/RunPulseSquintTrials.m
@@ -366,8 +366,13 @@ if any(cellfun(@(x) sum(strcmp(x,'base')),protocolParams.myRoles))
 end
 
 if any(cellfun(@(x) sum(strcmp(x,'pupil')), protocolParams.myActions))
-    cameraTurnOnCommand = '/Applications/VLC\ 2.app/Contents/MacOS/VLC qtcapture://0xfa13300005a39230';
-                system(cameraTurnOnCommand)
+    cameraTurnOnCommand = '/Applications/VLC\ 2.app/Contents/MacOS/VLC qtcapture://0xfa13300005a39230 &';
+    [recordedErrorFlag, consoleOutput] = system(cameraTurnOnCommand);
+    commandwindow;
+    fprintf('- Setup the IR camera. Press <strong>Enter</strong> when complete and ready to move on.\n');
+    input('');
+    cameraTurnOffCommand = 'osascript -e ''quit app "VLC"''';
+    [recordedErrorFlag, consoleOutput] = system(cameraTurnOffCommand);
 end
 
 
