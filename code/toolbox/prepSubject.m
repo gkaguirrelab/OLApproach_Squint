a1c443c9a19e85a564b303474f3aa7944b0aa843
melanopsin <melanopsin@gka06.psych.upenn.edu>
2017-12-18 18:09:30 -0500

    changed some commenting
    note though that this function will become obsolete
    -in its place, we’re going to have each satelite execute a function to just turn on and display the output of each hardware instrument (checkAudio, for example)

--- code/toolbox/prepSubject.m
+++ code/toolbox/prepSubject.m
@@ -1,44 +1,51 @@
 function prepSubject(ol, protocolParams)
-% This function is intended to prepare the subject for the subsequent
-% experiment session. This includes making sure that the various pieces of
-% hardware are working, that the subject can tolerate the stimuli, and
-% scratch trials so the subject can get used to the trial structure. 
-
-% Each hardware check is performed by the computer that controls that piece
-% of hardware. First, the base computer will check the audio. This includes
-% running an abbreviated scratch trial with a full "listening" window. The
-% subject should be seated in the rig and prompted to say something during
-% the "listening" window. After the trial, a plot of the recorded audio
-% will be displayed on the base computer. If the operator is satisfied, the
-% routine moves onto checking the setup of the IR camera. This action is
-% performed by one of the satellites (monkfish/mac mini). Rather than run a
-% scratch trial, the routine simply opens the IR camera via the app VLC on the satellite compiuter.
-% This allows for the operator to setup the rig such that the subject's
-% pupil is properly recorded. The third hardware check will be the check
-% the EMG on the other satellite (gka33, the macbook air); this has not yet
-% been implemented.
-
-% After the hardware checks, the subjects will be shown three trials of
-% increasing melanopsin contrast (100% -> 200% -> 400%). This is meant to
-% ensure the subject can tolerate each step in melanopsin contrast. The
-% subject is required to prompt the base computer via a button press on the
-% controller to proceed to the next trial. This only shows Mel stimuli, but
-% we could also show LMS and/or light flux if we want to.
-
-% Finally, the subject undergoes practice trials. These trials are
-% identical in structure to the trials of the actual experiment. After each
-% trial, this code will display the recorded data from each instrument, but
-% this has not yet been implemented. Subjects can undego as many practice
-% trials as desired. Each practice trial is currently configured to display
-% a 100% contrast pulse of melanopsin-directed stimulation.
-
-% INPUT:
-%   - protocolParams: a structure describing the parameters of relevant
-%   experiment. The specifics of the protocolParams will be applied to
-%   create the practice trials (as well as to convey more general
-%   information about the session, including date, observerID, session
-%   number, which computer operates what, etc.)
-%   - ol: object to control the OneLight
+% Collect pseudo trials to test equipment and prepare the subject
+%
+% Description:
+%   This function is intended to prepare the subject for the subsequent
+%   experiment session. This includes making sure that the various pieces
+%   of hardware are working, that the subject can tolerate the stimuli, and
+%   scratch trials so the subject can get used to the trial structure.
+%
+%   Each hardware check is performed by the computer that controls that
+%   piece of hardware. First, the base computer will check the audio. This
+%   includes running an abbreviated scratch trial with a full "listening"
+%   window. The subject should be seated in the rig and prompted to say
+%   something during the "listening" window. After the trial, a plot of the
+%   recorded audio will be displayed on the base computer. If the operator
+%   is satisfied, the routine moves onto checking the setup of the IR
+%   camera. This action is performed by one of the satellites (monkfish/mac
+%   mini). Rather than run a scratch trial, the routine simply opens the IR
+%   camera via the app VLC on the satellite compiuter. This allows for the
+%   operator to setup the rig such that the subject's pupil is properly
+%   recorded. The third hardware check will be the check the EMG on the
+%   other satellite (gka33, the macbook air); this has not yet been
+%   implemented.
+%
+%   After the hardware checks, the subjects will be shown three trials of
+%   increasing melanopsin contrast (100% -> 200% -> 400%). This is meant to
+%   ensure the subject can tolerate each step in melanopsin contrast. The
+%   subject is required to prompt the base computer via a button press on
+%   the controller to proceed to the next trial. This only shows Mel
+%   stimuli, but we could also show LMS and/or light flux if we want to.
+%
+%   Finally, the subject undergoes practice trials. These trials are
+%   identical in structure to the trials of the actual experiment. After
+%   each trial, this code will display the recorded data from each
+%   instrument, but this has not yet been implemented. Subjects can undego
+%   as many practice trials as desired. Each practice trial is currently
+%   configured to display a 100% contrast pulse of melanopsin-directed
+%   stimulation.
+%
+% Inputs:
+%   protocolParams        - A structure describing the parameters of 
+%                           relevant experiment. The specifics of the
+%                           protocolParams will be applied to create the
+%                           practice trials (as well as to convey more
+%                           general information about the session,
+%                           including date, observerID, session number,
+%                           which computer operates what, etc.)
+%   ol                    - Object to control the OneLight
 
 
 % This function is still a work in process. Specifically, it needs a way in
