e0cd59dbcfd477b9a7ad619c967814c160eba13b
Harrison McAdams <harrison.mcadams@gmail.com>
2018-01-11 15:21:35 -0500

    now plots EMG output

--- code/toolbox/testEMG.m
+++ code/toolbox/testEMG.m
@@ -1,8 +1,22 @@
-function testEMG(protocolParams)
+function [plotFig] = testEMG(protocolParams)
 
 emgOutput = SquintRecordEMG(...
                 'recordingDurationSecs', 5, ...
                 'simulate', false, ...
                 'verbose', true);
+
+            
+plotFig = figure('name', 'plotFig');
+subplot(1,2,1)
+plot(emgOutput.timebase, emgOutput.response(1,:));
+xlabel('Time (s)')
+ylabel('Voltage ***?')
+title('*** Leads')
+
+subplot(1,2,2)
+plot(emgOutput.timebase, emgOutput.response(1,:));
+xlabel('Time (s)')
+ylabel('Voltage ***?')
+title('*** Leads')
             
 end
