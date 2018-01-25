bf2889bea88d4354394fb1da574094adb3ee1660
melanopsin <melanopsin@gka06.psych.upenn.edu>
2018-01-25 11:47:26 -0500

    changes to reflect actual experiment design
    -added relevant calibration
    -changed pupil size of background and directions

--- code/protocols/SetupSquintApproach.m
+++ code/protocols/SetupSquintApproach.m
@@ -13,13 +13,13 @@
 approachParams.approach = 'OLApproach_Squint';
 
 % List of all calibrations used in this approach
-approachParams.calibrationTypes = {'BoxBRandomizedLongCableDStubby1_ND00'};
+approachParams.calibrationTypes = {'BoxBRandomizedLongCableDStubby1_ND00', 'BoxARandomizedLongCableAEyePiece1_ND01'};
 
 % List of all backgrounds used in this approach
-approachParams.backgroundNames = {'MelanopsinDirected_275_80_667', 'LMSDirected_275_80_667', 'LightFlux_540_380_50'};
+approachParams.backgroundNames = {'MelanopsinDirected_275_60_667', 'LMSDirected_275_60_667', 'LightFlux_540_380_50'};
 
 % List of all directions used in this approach
-approachParams.directionNames = {'MaxMel_275_80_667', 'MaxLMS_275_80_667', 'LightFlux_540_380_50'};
+approachParams.directionNames = {'MaxMel_275_60_667', 'MaxLMS_275_60_667', 'LightFlux_540_380_50'};
 
 %%  Make the backgrounds
 for cc = 1:length(approachParams.calibrationTypes)
