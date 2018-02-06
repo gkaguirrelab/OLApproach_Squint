37d4d52583b1f7f2aa15fc77148532c0f4cf6d71
harrison-mcadams <harrisonmcadams@huntsman-ve501-0794.apn.wlan.upenn.edu>
2018-02-06 15:09:56 -0500

    saves calibration to better spot
    -now learns about which subject and session we're working with
    -saves pupil calibration file appropriately

--- code/toolbox/testVideo.m
+++ code/toolbox/testVideo.m
@@ -12,15 +12,30 @@ cameraTurnOffCommand = 'osascript -e ''quit app "VLC"''';
 videoRecordSystemCommandStem='ffmpeg -hide_banner -video_size 1280x720 -framerate 60.000240 -f avfoundation -i "0" -c:v mpeg4 -q:v 1';
 
 
+% make the folder in which to save calibration
+% make a good guess of the which subject we're working with
+[ subjectID, sessionNumber ] = findMostRecentSession(protocolParams);
+subject = GetWithDefault('>> Enter <strong>observer name</strong>', subjectID);
+sessionName = GetWithDefault('>> Enter <strong>session number</strong>:', sessionNumber);
+
+
+
+
+todayDate = datestr(now, 'yyyy-mm-dd');
+outDir = fullfile(getpref(protocolParams.protocol, 'DataFilesBasePath'), subject, todayDate, sessionName, 'pupilCalibration');
+if ~exist(outDir)
+    mkdir(outDir); 
+end
+
 calibrationDoneFlag = false;
 counter = 1;
     while ~calibrationDoneFlag
         micCheckChoice = GetWithDefault('>> Run pupil calibration?? [y/n]', 'y');
         switch micCheckChoice
             case 'y'
-                pupilVideoSaveDirectoryPath = '~/Desktop/';
+                pupilVideoSaveDirectoryPath = outDir;
                 duration = 60;
-                videoOutFile = fullfile(pupilVideoSaveDirectoryPath, sprintf('calibration_%03d.avi',counter)); 
+                videoOutFile = fullfile(pupilVideoSaveDirectoryPath, sprintf('calibration_%03d_%s.avi',counter, subject)); 
                 videoRecordCommand = [protocolParams.videoRecordSystemCommandStem ' -t ' num2str(duration) ' ' videoOutFile ''];
                 [recordErrorFlag,consoleOutput]=system([videoRecordCommand , ' &']);
                 % audio routine to guide subject
