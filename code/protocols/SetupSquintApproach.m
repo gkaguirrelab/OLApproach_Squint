80a5118e9129376f23cdfcea0db8482ce2f8ad09
harrison-mcadams <harrisonmcadams@huntsman-ve501-0794.apn.wlan.upenn.edu>
2018-02-06 11:23:45 -0500

    adjustments for new experimtal approach
    -added light flux back
    -changed contrast for each stimulus type
    -set up multi-session structure

--- code/protocols/SetupSquintApproach.m
+++ code/protocols/SetupSquintApproach.m
@@ -13,13 +13,13 @@
 approachParams.approach = 'OLApproach_Squint';
 
 % List of all calibrations used in this approach
-approachParams.calibrationTypes = {'BoxBRandomizedLongCableDStubby1_ND00', 'BoxARandomizedLongCableAEyePiece1_ND01'};
+approachParams.calibrationTypes = {'BoxARandomizedLongCableAEyePiece1_ND01'};
 
 % List of all backgrounds used in this approach
-approachParams.backgroundNames = {'MelanopsinDirected_275_60_667', 'LMSDirected_275_60_667'};
+approachParams.backgroundNames = {'MelanopsinDirected_275_60_667', 'LMSDirected_275_60_667', 'LightFlux_540_380_50'};
 
 % List of all directions used in this approach
-approachParams.directionNames = {'MaxMel_unipolar_275_60_667', 'MaxLMS_unipolar_275_60_667'};
+approachParams.directionNames = {'MaxMel_unipolar_275_60_667', 'MaxLMS_unipolar_275_60_667', 'LightFlux_540_380_50'};
 
 %%  Make the backgrounds
 for cc = 1:length(approachParams.calibrationTypes)
