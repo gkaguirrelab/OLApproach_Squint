e7210a7650045520321e7ca01f8f19830eb1d967
harrison-mcadams <harrisonmcadams@huntsman-ve501-0794.apn.wlan.upenn.edu>
2018-02-07 11:27:33 -0500

    now finds most recent acquisitionNumber

--- code/toolbox/findMostRecentSession.m
+++ code/toolbox/findMostRecentSession.m
@@ -1,17 +1,30 @@
-function [ subjectID, sessionNumber ] = findMostRecentSession(protocolParams)
+function [ subjectID, sessionNumber, acquisitionNumber ] = findMostRecentSession(protocolParams)
 
+% find subject
 dataFilesDir = getpref(protocolParams.protocol, 'DataFilesBasePath');
 dirContent = dir(fullfile(dataFilesDir, '..', 'SessionRecords'));
 dirContent = dirContent(~ismember({dirContent.name},{'.','..', '.DS_Store'}));
 [value, index] = max([dirContent(:).datenum]);
-
 subjectID = dirContent(index).name;
+
+% get today's date
 todayDate = datestr(now, 'yyyy-mm-dd');
 
+% find session
 sessionDir = fullfile(dataFilesDir, '..', 'SessionRecords', subjectID, todayDate);
 sessionDirContent = dir(sessionDir);
 sessionDirContent = sessionDirContent(~ismember({sessionDirContent.name}, {'.','..', '.DS_Store'}));
 [value, index] = max([sessionDirContent(:).datenum]);
 sessionNumber = sessionDirContent(index).name;
+
+% find most recent acquisition
+dataDir = fullfile(dataFilesDir, subjectID, todayDate, sessionNumber)
+dataDirContent = dir(fullfile(dataDir, '*.mat'));
+[value, index] = max([dataDirContent(:).datenum]);
+acquisitionFile = dataDirContent(index).name;
+splits = strsplit(acquisitionFile, 'acquisition');
+acquisitionNumber = strsplit(splits{2}, '_');
+acquisitionNumber = str2num(acquisitionNumber{1});
+
 
 end
