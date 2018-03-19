8e2d79699c360a9cc3b3a12fc6eba9318abc38b7
melanopsin <melanopsin@monkfish.psych.upenn.edu>
2018-03-19 13:41:11 -0400

    turned off IR pulse

--- code/toolbox/SquintTrialLoop.m
+++ code/toolbox/SquintTrialLoop.m
@@ -440,7 +440,7 @@ for trial = 1:protocolParams.nTrials
         events(trial).tRecordingStartClock = clock;
         
         % turn on IR LED for 4-s
-        u3IR('open_sendTTL');
+        %u3IR('open_sendTTL');
         
         % ACTIONS -- emg
         if any(cellfun(@(x) sum(strcmp(x,'emg')), protocolParams.myActions))
