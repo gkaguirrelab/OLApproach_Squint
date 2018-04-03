19d749a0a1d589e3d9cb9bc81accc22911c9adb2
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-03 13:57:53 -0400

    new function
    brings the relevant data from cacheData generated through old spectrum
    seeking code to the directionObjects of the new code

---
+++ code/toolbox/applyCorrectedCachetoDirectionObjects.m
@@ -0,0 +1,9 @@
+function [DirectionObject, BackgroundObject] = applyCorrectedCachetoDirectionObjects(cacheData, DirectionObject, BackgroundObject)
+
+observerAge = DirectionObject.describe.observerAge;
+
+DirectionObject.differentialPrimaryValues = cacheData.data(observerAge).differencePrimary;
+BackgroundObject.differentialPrimaryValues = cacheData.data(observerAge).backgroundPrimary;
+DirectionObject.describe.correction = cacheData.data(32).correction;
+
+end
