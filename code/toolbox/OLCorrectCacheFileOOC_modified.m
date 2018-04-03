1c3dce0cc6cadf4c67b429397abbf94962e75201
melanopsin <melanopsin@gka06.psych.upenn.edu>
2018-04-03 13:40:11 -0400

    removed opening of radiometer
    that happens at the higher level in the rest of the approach code

--- code/toolbox/OLCorrectCacheFileOOC_modified.m
+++ code/toolbox/OLCorrectCacheFileOOC_modified.m
@@ -86,60 +86,60 @@ end
 persistent S
 persistent nAverage
 persistent theMeterTypeID
-
-if (isempty(spectroRadiometerOBJ))
-    % Open up the radiometer if this is the first cache file we validate
-    try
-        switch (meterType)
-            case 'PR-650',
-                theMeterTypeID = 1;
-                S = [380 4 101];
-                nAverage = 1;
-                
-                % Instantiate a PR650 object
-                spectroRadiometerOBJ  = PR650dev(...
-                    'verbosity',        1, ...       % 1 -> minimum verbosity
-                    'devicePortString', [] ...       % empty -> automatic port detection)
-                    );
-                spectroRadiometerOBJ.setOptions('syncMode', 'OFF');
-                
-            case 'PR-670',
-                theMeterTypeID = 5;
-                S = [380 2 201];
-                nAverage = 1;
-                
-                % Instantiate a PR670 object
-                spectroRadiometerOBJ  = PR670dev(...
-                    'verbosity',        1, ...       % 1 -> minimum verbosity
-                    'devicePortString', [] ...       % empty -> automatic port detection)
-                    );
-                
-                % Set options Options available for PR670:
-                spectroRadiometerOBJ.setOptions(...
-                    'verbosity',        1, ...
-                    'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];
-                    'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
-                    'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
-                    'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
-                    'apertureSize',     '1 DEG' ...     % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
-                    );
-            otherwise,
-                error('Unknown meter type');
-        end
-        
-    catch err
-        if (~isempty(spectroRadiometerOBJ))
-            spectroRadiometerOBJ.shutDown();
-            openSpectroRadiometerOBJ = [];
-        end
-        SendEmail(emailRecipient, 'OLValidateCacheFileOOC Failed', ...
-            ['Calibration failed with the following error' 10 err.message]);
-        keyboard;
-        rethrow(err);
-    end
-    
-    
-end
+% 
+% if (isempty(spectroRadiometerOBJ))
+%     % Open up the radiometer if this is the first cache file we validate
+%     try
+%         switch (meterType)
+%             case 'PR-650',
+%                 theMeterTypeID = 1;
+%                 S = [380 4 101];
+%                 nAverage = 1;
+%                 
+%                 % Instantiate a PR650 object
+%                 spectroRadiometerOBJ  = PR650dev(...
+%                     'verbosity',        1, ...       % 1 -> minimum verbosity
+%                     'devicePortString', [] ...       % empty -> automatic port detection)
+%                     );
+%                 spectroRadiometerOBJ.setOptions('syncMode', 'OFF');
+%                 
+%             case 'PR-670',
+%                 theMeterTypeID = 5;
+%                 S = [380 2 201];
+%                 nAverage = 1;
+%                 
+%                 % Instantiate a PR670 object
+%                 spectroRadiometerOBJ  = PR670dev(...
+%                     'verbosity',        1, ...       % 1 -> minimum verbosity
+%                     'devicePortString', [] ...       % empty -> automatic port detection)
+%                     );
+%                 
+%                 % Set options Options available for PR670:
+%                 spectroRadiometerOBJ.setOptions(...
+%                     'verbosity',        1, ...
+%                     'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];
+%                     'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
+%                     'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
+%                     'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
+%                     'apertureSize',     '1 DEG' ...     % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
+%                     );
+%             otherwise,
+%                 error('Unknown meter type');
+%         end
+%         
+%     catch err
+%         if (~isempty(spectroRadiometerOBJ))
+%             spectroRadiometerOBJ.shutDown();
+%             openSpectroRadiometerOBJ = [];
+%         end
+%         SendEmail(emailRecipient, 'OLValidateCacheFileOOC Failed', ...
+%             ['Calibration failed with the following error' 10 err.message]);
+%         keyboard;
+%         rethrow(err);
+%     end
+%     
+%     
+% end
 openSpectroRadiometerOBJ = spectroRadiometerOBJ;
 
 % Attempt to open the LabJack temperature sensing device
@@ -230,7 +230,7 @@ end
 % % Omni. Therefore, we read out the toggle directly from the calibration
 % % file. First entry is PR-6xx and is always true. Second entry is omni and
 % % can be on or off, depending on content of calibration.
-% meterToggle = [1 cal.describe.useOmni];
+meterToggle = [1 cal.describe.useOmni];
 % 
 % % Setup the OLCache object.
 % olCache = OLCache(cacheDir, cal);
@@ -471,7 +471,7 @@ try
                 
                 % Save out information about the correction
                 [contrasts(:,iter) postreceptoralContrasts(:,iter)] = ComputeAndReportContrastsFromSpds(['Iteration ' num2str(iter, '%02.0f')] ,theCanonicalPhotoreceptors,T_receptors,...
-                    results.modulationBGMeas.meas.pr650.spectrum,results.modulationMaxMeas.meas.pr650.spectrum,describe.postreceptoralCombinations,true);
+                    results.modulationBGMeas.meas.pr650.spectrum,results.modulationMaxMeas.meas.pr650.spectrum);
                 
                 backgroundPrimaryCorrectedAll(:,iter) = backgroundPrimaryCorrected;
                 deltaBackgroundPrimaryInferredAll(:,iter)= deltaBackgroundPrimaryInferred;
