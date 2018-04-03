9714c6fa9828b06f24b6c706766675e44f3b5e94
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-03 13:03:35 -0400

    new function to debug implementation of old spectrum seeking

---
+++ code/toolbox/spectrumSeekingTest.m
@@ -0,0 +1,57 @@
+protocolParams.boxName = 'BoxB';
+protocolParams.calibrationType = 'BoxBRandomizedLongCableAEyePiece1_ND04';
+protocolParams.takeCalStateMeasurements = true;
+protocolParams.takeTemperatureMeasurements = false;
+
+% Get calibration
+calibration = OLGetCalibrationStructure('CalibrationType',protocolParams.calibrationType,'CalibrationDate','latest');
+
+% Validation parameters
+protocolParams.nValidationsPerDirection = 5;
+
+commandwindow;
+protocolParams.observerID = GetWithDefault('>> Enter <strong>observer name</strong>', 'HERO_xxxx');
+protocolParams.observerAgeInYrs = GetWithDefault('>> Enter <strong>observer age</strong>:', 32);
+protocolParams.sessionName = GetWithDefault('>> Enter <strong>session number</strong>:', 'session_1');
+protocolParams.todayDate = datestr(now, 'yyyy-mm-dd');
+
+ol = OneLight('simulate',protocolParams.simulate.oneLight,'plotWhenSimulating',protocolParams.simulate.makePlots); drawnow;
+
+%% Let user get the radiometer set up
+if ~protocolParams.simulate.radiometer
+    radiometerPauseDuration = 0;
+    ol.setAll(true);
+    commandwindow;
+    fprintf('- Focus the radiometer and press enter to pause %d seconds and start measuring.\n', radiometerPauseDuration);
+    input('');
+    ol.setAll(false);
+    pause(radiometerPauseDuration);
+    radiometer = OLOpenSpectroRadiometerObj('PR-670');
+else
+    radiometer = [];
+end
+
+MaxMelParams = OLDirectionParamsFromName('MaxMel_unipolar_275_60_667');
+[ MaxMelDirection, MaxMelBackground ] = OLDirectionNominalFromParams(MaxMelParams, calibration, 'observerAge',protocolParams.observerAgeInYrs);
+
+fakeMelCache = makeFakeCache(MaxMelDirection);
+
+[cacheData olCache spectroRadiometerOBJ] = OLCorrectCacheFileOOC_modified(...
+        fakeMelCache, calibration, ...
+        'igdalova@mail.med.upenn.edu', ...
+        'PR-670', spectroRadiometerOBJ, spectroRadiometerOBJWillShutdownAfterMeasurement, ...
+        'FullOnMeas', false, ...
+        'CalStateMeas', false, ...
+        'DarkMeas', false, ...
+        'OBSERVER_AGE', observerAgeInYrs, ...
+        'ReducedPowerLevels', false, ...
+        'selectedCalType', theCalType, ...
+        'CALCULATE_SPLATTER', false, ...
+        'lambda', 0.8, ...
+        'NIter', 20, ...
+        'powerLevels', [0 1.0000], ...
+        'doCorrection', theDirectionsCorrect(d), ...
+        'postreceptoralCombinations', [1 1 1 0 ; 1 -1 0 0 ; 0 0 1 0 ; 0 0 0 1], ...
+        'outDir', fullfile(materialsPath, 'PIPRMaxPulse', todayDate), ...
+        'takeTemperatureMeasurements', takeTemperatureMeasurements);
+    fprintf(' * Spectrum seeking finished!\n')
