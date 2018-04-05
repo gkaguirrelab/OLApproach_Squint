81b7cfdf8d02d22c33c3419bb424cda39c17233b
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-04-05 13:58:53 -0400

    Moved corrections function to OLToolbox

--- code/toolbox/applyCorrectedCachetoDirectionObjects.m
+++
@@ -1,9 +0,0 @@
-function applyCorrectedCachetoDirectionObjects(cacheData, DirectionObject, BackgroundObject)
-
-observerAge = DirectionObject.describe.observerAge;
-
-DirectionObject.differentialPrimaryValues = cacheData.data(observerAge).differencePrimary;
-BackgroundObject.differentialPrimaryValues = cacheData.data(observerAge).backgroundPrimary;
-DirectionObject.describe.correction = cacheData.data(32).correction;
-
-end
