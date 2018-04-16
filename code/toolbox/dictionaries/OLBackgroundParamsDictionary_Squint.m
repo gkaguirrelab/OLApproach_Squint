87ffab22e873574d5938ae86b4e70f042324fd36
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-16 16:26:47 -0400

    local dictionaries!
    define light flux modulations, or at least their beginnings

---
+++ code/toolbox/dictionaries/OLBackgroundParamsDictionary_Squint.m
@@ -0,0 +1,264 @@
+function dictionary = OLBackgroundParamsDictionary(varargin)
+% Defines a dictionary with parameters for named nominal backgrounds
+%
+% Syntax:
+%   dictionary = OLBackgroundParamsDictionary()
+%
+% Description:
+%    Define a dictionary of named backgrounds of modulation, with
+%    corresponding nominal parameters.
+%
+% Inputs:
+%    None.
+%
+% Outputs:
+%    dictionary         -  Dictionary with all parameters for all desired
+%                          backgrounds
+%
+% Optional key/value pairs:
+%    'alternateDictionaryFunc' - String with name of alternate dictionary
+%                          function to call. This must be a function on the
+%                          path. Default of empty results in using this
+%                          function.
+%
+% Notes:
+%    * TODO:
+%          i) add type 'BackgroundHalfOn' - Primaries set to 0.5;
+%          ii) add type 'BackgroundEES' - Background metameric to an equal 
+%              energy spectrum, scaled in middle of gamut.
+%
+% See also: 
+%    OLBackgroundParams, OLDirectionParamsDictionary.
+
+% History:
+%    06/28/17  dhb  Created from direction version.
+%    06/28/18  dhb  backgroundType -> backgroundName. Use names of routine 
+%                   that creates backgrounds.
+%              dhb  Add name field.
+%              dhb  Bring in params.photoreceptorClasses.  These go with 
+%                   directions/backgrounds.
+%              dhb  Bring in params.useAmbient.  This goes with directions/
+%                   backgrounds.
+%    06/29/18  dhb  More extended names to reflect key parameters, so that 
+%                   protocols can check
+%    07/19/17  npc  Added a type for each background. For now, there is 
+%                   only one type: 'basic'. 
+%                   Defaults and checking are done according to type. 
+%                   params.photoreceptorClasses is now a cell array.
+%    07/22/17  dhb  No more modulationDirection field.
+%    01/25/18  jv   Extract default params generation, validation.
+%    02/07/18  jv   Updated to use OLBackgroundParams objects
+%    03/26/18  jv, dhb Fix type in modulationContrast field of
+%                   LMSDirected_LMS_275_60_667.
+%    03/31/18  dhb  Add alternateDictionaryFunc key/value pair.
+%              dhb  Delete obsolete notes and see alsos.
+%    04/09/18  dhb  Removing light flux parameters. Use a local dictionary!
+
+% Parse input
+p = inputParser;
+p.KeepUnmatched = true;
+p.addParameter('alternateDictionaryFunc','',@ischar);
+p.parse(varargin{:});
+
+% Check for alternate dictionary, call if so and then return.
+% Otherwise this is the dictionary function and we execute it.
+% The alternate function must be on the path.
+if (~isempty(p.Results.alternateDictionaryFunc))
+    dictionaryFunction = str2func(sprintf('@%s',p.Results.alternateDictionaryFunc));
+    dictionary = dictionaryFunction();
+    return;
+end
+
+% Initialize dictionary
+dictionary = containers.Map();
+
+%% MelanopsinDirected_275_80_667
+% Background to allow maximum unipolar contrast melanopsin modulation
+%   Field size: 27.5 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'MelanopsinDirected';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 4/6;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 8;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = 4/6;
+params.whichReceptorsToIsolate = {[4]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [0];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MelanopsinDirected_600_80_667
+% Background to allow maximum unipolar contrast melanopsin modulations
+%   Field size: 60.0 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'MelanopsinDirected';
+params.baseModulationContrast = 4/6;
+params.primaryHeadRoom = 0.01;
+params.fieldSizeDegrees = 60;
+params.pupilDiameterMm = 8;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [4/6];
+params.whichReceptorsToIsolate = {[4]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [0];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% LMSDirected_LMS_275_80_667
+% Background to allow maximum unipolar contrast LMS modulations
+%   Field size: 27.5 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'LMSDirected';
+params.baseModulationContrast = 4/6;
+params.primaryHeadRoom = 0.005;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 8;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = {[4/6 4/6 4/6]};
+params.whichReceptorsToIsolate = {[1 2 3]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [1];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% LMSDirected_LMS_600_80_667
+% Background to allow maximum unipolar contrast LMS modulations
+%   Field size: 60.0 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'LMSDirected';
+params.baseModulationContrast = 4/6;
+params.primaryHeadRoom = 0.005;
+params.fieldSizeDegrees = 60;
+params.pupilDiameterMm = 8;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = {[4/6 4/6 4/6]};
+params.whichReceptorsToIsolate = {[1 2 3]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [1];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MelanopsinDirected_275_60_667
+% Background to allow maximum unipolar contrast melanopsin modulations
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'MelanopsinDirected';
+params.baseModulationContrast = 4/6;
+params.primaryHeadRoom = 0.00;
+params.pupilDiameterMm = 6;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [4/6];
+params.whichReceptorsToIsolate = {[4]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [0];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% LMSDirected_LMS_275_60_667
+% Background to allow maximum unipolar contrast LMS modulations
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, this background is also used
+% for a 400% unipolar pulse
+params = OLBackgroundParams_Optimized;
+params.baseName = 'LMSDirected';
+params.baseModulationContrast = 4/6;
+params.primaryHeadRoom = 0.00;
+params.pupilDiameterMm = 6;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = {[4/6 4/6 4/6]};
+params.whichReceptorsToIsolate = {[1 2 3]};
+params.whichReceptorsToIgnore = {[]};
+params.whichReceptorsToMinimize = {[]};
+params.directionsYoked = [1];
+params.directionsYokedAbs = [0];
+params.name = OLBackgroundNameFromParams(params);
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% LightFlux_UnipolarBase
+%
+% Base params for unipolar light flux modulation backgrounds
+params = OLBackgroundParams_LightFluxChrom;
+params.baseName = 'LightFlux';
+params.polarType = 'unipolar';
+params.desiredxy = [0.595,0.375];
+params.whichXYZ = 'xyzCIEPhys10';
+params.desiredMaxContrast = 4;
+
+% These are the options that go to OLPrimaryInvSolveChrom
+params.search.primaryHeadRoom = 0.005;
+params.search.primaryTolerance = 1e-6;
+params.search.checkPrimaryOutOfRange = true;
+params.search.initialLuminanceFactor = 0.2;
+params.search.lambda = 0;
+params.search.spdToleranceFraction = 0.005;
+params.search.chromaticityTolerance = 0.0001;
+params.search.optimizationTarget = 'maxLum';
+params.search.primaryHeadroomForInitialMax = 0.005;
+params.search.maxScaleDownForStart = 2;
+params.search.maxSearchIter = 300;
+params.search.verbose = false;
+
+params.name = 'LightFlux_UnipolarBase';
+if OLBackgroundParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+end
---
+++ code/toolbox/dictionaries/OLDirectionParamsDictionary_Squint.m
@@ -0,0 +1,378 @@
+function dictionary = OLDirectionParamsDictionary_Squint(varargin)
+% Defines a dictionary with parameters for named nominal directions
+%
+% Syntax:
+%   dictionary = OLDirectionParamsDictionary()
+%
+% Description:
+%    Define a dictionary of named directions of modulation, with
+%    corresponding nominal parameters. Types of directions, and their
+%    corresponding fields, are defined in OLDirectionParamsDefaults,
+%    and validated by OLDirectionParamsValidate.
+%
+% Inputs:
+%    None.
+%
+% Outputs:
+%    dictionary - dictionary with all parameters for all desired directions
+%
+% Optional key/value pairs:
+%    'alternateDictionaryFunc' - String with name of alternate dictionary
+%                          function to call. This must be a function on the
+%                          path. Default of empty results in using this
+%                          function.
+%
+% Notes:
+%    None.
+%
+% See also: OLBackgroundParamsDictionary
+
+% History:
+%    06/22/17  npc  Wrote it. 06/28/18  dhb  backgroundType ->
+%                   backgroundName. Use names of routine that creates
+%                   backgrounds.
+%              dhb  Add name field. 
+%              dhb  Explicitly set contrasts in each case, rather than rely
+%                   on defaults. 
+%              dhb  Bring in params.photoreceptorClasses.  These go with
+%                   directions/backgrounds. 
+%              dhb  Bring in params.useAmbient. This goes with directions/
+%                   backgrounds.
+%    07/05/17  dhb  Bringing up to speed. :
+%    07/19/17  npc  Added a type for each background. For now, there is 
+%                   only one type: 'pulse'. Defaults and checking are done 
+%                   according to type. params.photoreceptorClasses is now a
+%                   cell array
+%    07/22/17  dhb  No more modulationDirection field. 
+%    07/23/17  dhb  Comment field meanings. 
+%    07/27/17  dhb  Light flux entry 
+%    01/24/18  dhb,jv  Finished adding support for modulations
+%              jv   Renamed direction types: pulse is now unipolar,
+%                   modulation is now bipolar
+%	 01/25/18  jv	Extract defaults generation, validation of params.
+%    02/15/18  jv   Parameters are now objects
+%    03/31/18  dhb  Add alternateDictionaryFunc key/value pair.
+%              dhb  Delete obsolete notes and see alsos.
+%    04/09/18  dhb  Removing light flux parameters. Use a local dictionary!
+
+% Parse input
+p = inputParser;
+p.KeepUnmatched = true;
+p.addParameter('alternateDictionaryFunc','',@ischar);
+p.parse(varargin{:});
+
+% Check for alternate dictionary, call if so and then return.
+% Otherwise this is the dictionary function and we execute it.
+% The alternate function must be on the path.
+if (~isempty(p.Results.alternateDictionaryFunc))
+    dictionaryFunction = str2func(sprintf('@%s',p.Results.alternateDictionaryFunc));
+    dictionary = dictionaryFunction();
+    return;
+end
+
+%% Initialize dictionary
+dictionary = containers.Map();
+
+%% MaxMel_unipolar_275_80_667
+% Direction for maximum unipolar contrast melanopsin step
+%   Field size: 27.5 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% 
+% unipolar contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxMel';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 8.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_275_80_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxMel_bipolar_275_80_667
+% Direction for maximum bipolar contrast melanopsin modulation
+%   Field size: 27.5 deg
+%   Pupil diameter: 8 mm
+%   Bipolar contrast: 66.7%
+params = OLDirectionParams_Bipolar;
+params.baseName = 'MaxMel';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 8.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_275_80_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxMel_unipolar_275_60_667
+% Direction for maximum unipolar contrast melanopsin step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil as part of
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxMel';
+params.primaryHeadRoom = 0.0;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxMel_unipolar_275_60_500
+% Direction for maximum unipolar contrast melanopsin step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil as part of
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxMel';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 1/2;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxMel_unipolar_600_80_667
+% Direction for maximum unipolar contrast melanopsin step
+%   Field size: 60.0 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxMel';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 60.0;
+params.pupilDiameterMm = 8.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_600_80_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxLMS_unipolar_275_80_667
+% Direction for maximum unipolar contrast LMS step
+%   Field size: 27.5 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxLMS';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 8.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1 2 3];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'LMSDirected_275_80_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxLMS_unipolar_275_60_667
+% Direction for maximum unipolar contrast LMS step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil with
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxLMS';
+params.primaryHeadRoom = 0.0;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1 2 3];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'LMSDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxLMS_unipolar_275_60_500
+% Direction for maximum unipolar contrast LMS step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil with
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxLMS';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 1/2;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1 2 3];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'LMSDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxLMS_bipolar_275_60_667
+% Direction for maximum bipolar contrast LMS step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil with
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+params = OLDirectionParams_Bipolar;
+params.baseName = 'MaxLMS';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1 2 3];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'LMSDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxLMS_unipolar_600_80_667
+% Direction for maximum unipolar contrast LMS step
+%   Field size: 60.0 deg
+%   Pupil diameter: 8 mm
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxLMS';
+params.primaryHeadRoom = 0.01;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 60.0;
+params.pupilDiameterMm = 8.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1 2 3];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'LMSDirected_600_80_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+%% MaxMelLMS_unipolar_275_60_667
+% Direction for maximum unipolar contrast melanopsin step
+%   Field size: 27.5 deg
+%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil as part of
+%   pupillometry
+%   bipolar contrast: 66.7%
+%
+% Bipolar contrast is specified to generate, but the result is a 400% unipolar
+% contrast step up relative to the background.
+params = OLDirectionParams_Unipolar;
+params.baseName = 'MaxMelLMS';
+params.primaryHeadRoom = 0.0;
+params.baseModulationContrast = 2/3;
+params.fieldSizeDegrees = 27.5;
+params.pupilDiameterMm = 6.0;
+params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
+params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
+params.whichReceptorsToIsolate = [1, 2, 3, 4];
+params.whichReceptorsToIgnore = [];
+params.whichReceptorsToMinimize = [];
+params.backgroundName = 'MelanopsinDirected_275_60_667';
+params.name = OLDirectionNameFromParams(params);
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+%% LightFlux_UnipolarBase
+%
+% Base params for unipolar light flux directions
+params = OLDirectionParams_LightFluxChrom;
+params.baseName = 'LightFlux';
+params.polarType = 'unipolar';
+params.desiredxy = [0.595,0.375];
+params.whichXYZ = 'xyzCIEPhys10';
+params.desiredMaxContrast = 4;
+params.name = 'LightFlux_UnipolarBase';
+params.backgroundName = 'LightFlux_UnipolarBase';
+if OLDirectionParamsValidate(params)
+    % All validations OK. Add entry to the dictionary.
+    dictionary(params.name) = params;
+end
+
+end
