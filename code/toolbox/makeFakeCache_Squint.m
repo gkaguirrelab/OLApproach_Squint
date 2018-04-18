e833ac43bc74191b421b5e1d9bd870b855839eed
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-18 14:04:37 -0400

    bringing back PIPR correction code

---
+++ code/toolbox/makeFakeCache_Squint.m
@@ -0,0 +1,12 @@
+function [ fakeCache ] = makeFakeCache_Squint(DirectionObject)
+
+observerAge = DirectionObject.describe.observerAge;
+
+fakeCache.computeMethod = 'ReceptorIsolate';
+fakeCache.data(observerAge).params.receptorIsolateMode = 'Standard';
+fakeCache.data(observerAge).backgroundPrimary = DirectionObject.describe.backgroundNominal.differentialPrimaryValues;
+fakeCache.data(observerAge).differencePrimary = DirectionObject.differentialPrimaryValues;
+fakeCache.data(observerAge).describe.photoreceptors = DirectionObject.describe.directionParams.photoreceptorClasses;
+fakeCache.data(observerAge).describe.T_receptors = DirectionObject.describe.directionParams.T_receptors;
+
+end
