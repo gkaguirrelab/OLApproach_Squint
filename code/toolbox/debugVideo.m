ffca1e9a92142164695b2fb8c872420e8b08b67c
melanopsin <melanopsin@monkfish.psych.upenn.edu>
2018-05-01 12:44:31 -0400

    up and running

--- code/toolbox/debugVideo.m
+++ code/toolbox/debugVideo.m
@@ -1,13 +1,21 @@
 protocolParams.protocol = 'SquintToPulse';
-protocolParams.videoRecordSystemCommandStem=['ffmpeg -hide_banner -video_size 1280x720 -pix_fmt uyvy422 -framerate 60.000240 -f avfoundation -i "0" -c:v mpeg4 -q:v 1'];
+protocolParams.videoRecordSystemCommandStem=['ffmpeg -hide_banner -video_size 1280x720 -pix_fmt uyvy422 -vsync 0 -framerate 60.000240 -f avfoundation -i "0" -c:v mpeg4 -q:v 1'];
 pupilVideoSaveDirectoryPath = fullfile(getpref(protocolParams.protocol, 'DataFilesBasePath'), 'cameraDebugging');
 
-potentialVideoFiles = dir(pupilVideoSaveDirectoryPath, 'sample*');
-potentialVideoFiles = potentialVideoFiles.names;
+potentialVideoFiles = dir(fullfile(pupilVideoSaveDirectoryPath, 'sample*'));
 
-counter = 1 + length(potentialVideoFiles);
+if length(potentialVideoFiles) == 0
+    counter = 1;
+else
+    
+    counter = 1 + length(potentialVideoFiles);
+end
+
+if ~exist(pupilVideoSaveDirectoryPath,'dir')
+    mkdir(pupilVideoSaveDirectoryPath);
+end
 
 
-videoOutFile = fullfile(pupilVideoSaveDirectoryPath, sprintf('sample_%03d.mp4',counter));
-videoRecordCommand = [protocolParams.videoRecordSystemCommandStem ' -t ' num2str(theMessageReceived.data.duration) ' "' videoOutFile '"'];
+videoOutFile = fullfile(pupilVideoSaveDirectoryPath, sprintf('sample_vsync0_%03d.mp4',counter));
+videoRecordCommand = [protocolParams.videoRecordSystemCommandStem ' -t 17' ' "' videoOutFile '"'];
 [recordErrorFlag,consoleOutput]=system(videoRecordCommand);
