8200153a8a356baa184460d723a046edae7db978
npcottaris <cottaris@sas.upenn.edu>
2018-07-03 13:24:25 -0400

    Bug fix that doesn’t need to be pushed

--- code/toolbox/tortureTest.m
+++ code/toolbox/tortureTest.m
@@ -2,7 +2,7 @@ function tortureTest
 
 [protocolParams] = getDefaultParams('calibrationType', 'BoxALiquidLightGuideCEyePiece2ND01');
 
-for ii = 2:1000
+for ii = 7:1000
     sessionName = 'session_1';
     observerAgeInYrs = 32;
     observerID = ['tortureTest_', num2str(ii)];
@@ -11,5 +11,6 @@ for ii = 2:1000
     try 
         radiometer.shutDown 
     end
+    close all
 end
 end
