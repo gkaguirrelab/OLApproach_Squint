8d8eb9a34ece456762fb5120292947eecd59da4e
harrison-mcadams <harrison.mcadams@gmail.com>
2018-07-05 10:40:12 -0400

    updated to current method of light flux generation

--- code/toolbox/developLightFlux.m
+++ code/toolbox/developLightFlux.m
@@ -1,3 +1,5 @@
+clear all
+
 protocolParams.simulate.oneLight = true;
 protocolParams.simulate.radiometer = true;
 protocolParams.simulate.makePlots = false;
@@ -6,8 +8,8 @@ protocolParams.observerAgeInYrs = 32;
 
 
 % new stuff
-backgroundAlternateDictionary = 'OLBackgroundParamsDictionary_Squint';
-directionAlternateDictionary = 'OLDirectionParamsDictionary_Squint';
+protocolParams.backgroundDictionary = 'OLBackgroundParamsDictionary_Squint';
+protocolParams.directionsDictionary = 'OLDirectionParamsDictionary_Squint';
 % whichXYZ = 'xyzCIEPhys10';
 % eval(['tempXYZ = load(''T_' whichXYZ ''');']);
 % eval(['T_xyz = SplineCmf(tempXYZ.S_' whichXYZ ',683*tempXYZ.T_' whichXYZ ',calibration.describe.S);']);
@@ -16,8 +18,8 @@ directionAlternateDictionary = 'OLDirectionParamsDictionary_Squint';
 % nativexy = nativexyY(1:2);
 
 % OneLight parameters
-protocolParams.boxName = 'BoxB';
-protocolParams.calibrationType = 'BoxBShortLiquidLightGuideDEyePiece1_ND04';
+protocolParams.boxName = 'BoxA';
+protocolParams.calibrationType = 'BoxALiquidLightGuideCEyePiece2ND01';
 protocolParams.takeCalStateMeasurements = true;
 protocolParams.takeTemperatureMeasurements = false;
 calibration = OLGetCalibrationStructure('CalibrationType',protocolParams.calibrationType,'CalibrationDate','latest');
@@ -26,15 +28,39 @@ calibration = OLGetCalibrationStructure('CalibrationType',protocolParams.calibra
 radiometer = [];
 ol = OneLight('simulate',protocolParams.simulate.oneLight,'plotWhenSimulating',protocolParams.simulate.makePlots); drawnow;
 
-LightFluxParams = OLDirectionParamsFromName('LightFlux_UnipolarBase', 'alternateDictionaryFunc', directionAlternateDictionary);
-LightFluxParams.backgroundParams = OLBackgroundParamsFromName(LightFluxParams.backgroundName, 'alternateDictionaryFunc', backgroundAlternateDictionary);
-
-[LightFluxDirection, LightFluxBackground ] = OLDirectionNominalFromParams(LightFluxParams, calibration);
-
+ LightFluxParams = OLDirectionParamsFromName('LightFlux_UnipolarBase', 'alternateDictionaryFunc', protocolParams.directionsDictionary);
+    
+    % playing around with the light flux params -- these are the specific
+    % parameters David played with. with the most recent calibration for BoxD
+    % with the short liquid light guide and ND0.1, these gave reasonable
+    % modulations
+    
+    whichXYZ = 'xyzCIEPhys10';
+    LightFluxParams.desiredxy = [0.51 0.40];
+    LightFluxParams.whichXYZ = whichXYZ;
+    LightFluxParams.desiredMaxContrast = 4;
+    LightFluxParams.desiredBackgroundLuminance = 1129;
+    
+    LightFluxParams.search.primaryHeadroom = 0.000;
+    LightFluxParams.search.primaryTolerance = 1e-6;
+    LightFluxParams.search.checkPrimaryOutOfRange = true;
+    LightFluxParams.search.lambda = 0;
+    LightFluxParams.search.spdToleranceFraction = 30e-5;
+    LightFluxParams.search.chromaticityTolerance = 0.001;
+    LightFluxParams.search.optimizationTarget = 'maxContrast';
+    LightFluxParams.search.primaryHeadroomForInitialMax = 0.000;
+    LightFluxParams.search.maxSearchIter = 3000;
+    LightFluxParams.search.verbose = false;
+    
+    [ LightFluxDirection, LightFluxBackground ] = OLDirectionNominalFromParams(LightFluxParams, calibration);
+    LightFluxDirection.describe.observerAge = protocolParams.observerAgeInYrs;
+   
 
 MaxMelParams = OLDirectionParamsFromName('MaxMel_unipolar_275_60_667');
 [ MaxMelDirection, MaxMelBackground ] = OLDirectionNominalFromParams(MaxMelParams, calibration, 'observerAge',protocolParams.observerAgeInYrs);
 T_receptors = MaxMelDirection.describe.directionParams.T_receptors; % the T_receptors will be the same for each direction, so just grab one
+ LightFluxDirection.describe.photoreceptorClasses = MaxMelDirection.describe.directionParams.photoreceptorClasses;
+    LightFluxDirection.describe.T_receptors = MaxMelDirection.describe.directionParams.T_receptors;
 
 
 OLValidateDirection(LightFluxDirection, LightFluxBackground, ol, radiometer, 'receptors', T_receptors, 'label', 'precorrection')
