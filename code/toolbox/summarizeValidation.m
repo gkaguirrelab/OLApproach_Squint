1414f4dee1ef3a675220037f5861a4e29a73c751
harrison-mcadams <harrison.mcadams@gmail.com>
2018-07-11 16:12:46 -0400

    will now also accumulate temperature, if measured

--- code/toolbox/summarizeValidation.m
+++ code/toolbox/summarizeValidation.m
@@ -39,6 +39,13 @@ for vv = validationIndices
     %validation.backgroundLuminance(vv) = DirectionObject.describe.(potentialValidations{vv}).actualBackgroundLuminance;
     
     validation.backgroundLuminance(counter) = DirectionObject.describe.validation(vv).luminanceActual(1);
+    if isfield(DirectionObject.describe.validation(vv), 'temperatures')
+        validation.boxTemperature(counter) = DirectionObject.describe.validation(vv).temperatures{1}.value(1);
+        validation.roomTemperature(counter) = DirectionObject.describe.validation(vv).temperatures{1}.value(2);
+    else
+        validation.boxTemperature(counter) = NaN;
+        validation.roomTemperature(counter) = NaN;
+    end
     counter = counter + 1;
 end
 
