2331def3141fad8e75541f8c7dd5f3bd64a5e479
melanopsin <melanopsin@gka06.psych.upenn.edu>
2018-07-17 11:10:36 -0400

    labeled Box D for output

--- code/toolbox/tortureTest.m
+++ code/toolbox/tortureTest.m
@@ -1,11 +1,11 @@
 function tortureTest
 
-[protocolParams] = getDefaultParams('calibrationType', 'BoxALiquidLightGuideCEyePiece2ND01');
+[protocolParams] = getDefaultParams('calibrationType', 'BoxDLiquidShortCableDEyePiece1_ND03');
 
-for ii = 7:1000
+for ii = 1:1000
     sessionName = 'session_1';
     observerAgeInYrs = 32;
-    observerID = ['tortureTest_', num2str(ii)];
+    observerID = ['tortureTest_boxD_', num2str(ii)];
     [ modulationData, ol, radiometer, calibration, protocolParams ] = prepExperiment(protocolParams, 'observerID', observerID, 'sessionName', sessionName, 'observerAgeInYrs', observerAgeInYrs, 'skipPause', true);
     if exist('radiometer', 'var')
     try 
