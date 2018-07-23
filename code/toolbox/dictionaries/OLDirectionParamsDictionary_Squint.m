f7287bd1af4748067645ce2e519b115529b76e84
harrison-mcadams <harrison.mcadams@gmail.com>
2018-07-23 14:25:01 -0400

    dictionaries stripped of unused entries

--- code/toolbox/dictionaries/OLDirectionParamsDictionary_Squint.m
+++ code/toolbox/dictionaries/OLDirectionParamsDictionary_Squint.m
@@ -73,54 +73,8 @@ end
 %% Initialize dictionary
 dictionary = containers.Map();
 
-%% MaxMel_unipolar_275_80_667
-% Direction for maximum unipolar contrast melanopsin step
-%   Field size: 27.5 deg
-%   Pupil diameter: 8 mm
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% 
-% unipolar contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxMel';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 8.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast];
-params.whichReceptorsToIsolate = [4];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'MelanopsinDirected_275_80_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxMel_bipolar_275_80_667
-% Direction for maximum bipolar contrast melanopsin modulation
-%   Field size: 27.5 deg
-%   Pupil diameter: 8 mm
-%   Bipolar contrast: 66.7%
-params = OLDirectionParams_Bipolar;
-params.baseName = 'MaxMel';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 8.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast];
-params.whichReceptorsToIsolate = [4];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'MelanopsinDirected_275_80_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
+
 
 %% MaxMel_unipolar_275_60_667
 % Direction for maximum unipolar contrast melanopsin step
@@ -149,84 +103,8 @@ if OLDirectionParamsValidate(params)
     dictionary(params.name) = params;
 end
 
-%% MaxMel_unipolar_275_60_500
-% Direction for maximum unipolar contrast melanopsin step
-%   Field size: 27.5 deg
-%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil as part of
-%   pupillometry
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxMel';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 1/2;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 6.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast];
-params.whichReceptorsToIsolate = [4];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'MelanopsinDirected_275_60_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxMel_unipolar_600_80_667
-% Direction for maximum unipolar contrast melanopsin step
-%   Field size: 60.0 deg
-%   Pupil diameter: 8 mm
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxMel';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 60.0;
-params.pupilDiameterMm = 8.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast];
-params.whichReceptorsToIsolate = [4];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'MelanopsinDirected_600_80_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxLMS_unipolar_275_80_667
-% Direction for maximum unipolar contrast LMS step
-%   Field size: 27.5 deg
-%   Pupil diameter: 8 mm
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxLMS';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 8.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
-params.whichReceptorsToIsolate = [1 2 3];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'LMSDirected_275_80_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
 %% MaxLMS_unipolar_275_60_667
 % Direction for maximum unipolar contrast LMS step
@@ -255,110 +133,11 @@ if OLDirectionParamsValidate(params)
     dictionary(params.name) = params;
 end
 
-%% MaxLMS_unipolar_275_60_500
-% Direction for maximum unipolar contrast LMS step
-%   Field size: 27.5 deg
-%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil with
-%   pupillometry
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxLMS';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 1/2;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 6.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
-params.whichReceptorsToIsolate = [1 2 3];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'LMSDirected_275_60_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxLMS_bipolar_275_60_667
-% Direction for maximum bipolar contrast LMS step
-%   Field size: 27.5 deg
-%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil with
-%   pupillometry
-%   bipolar contrast: 66.7%
-%
-params = OLDirectionParams_Bipolar;
-params.baseName = 'MaxLMS';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 6.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
-params.whichReceptorsToIsolate = [1 2 3];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'LMSDirected_275_60_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxLMS_unipolar_600_80_667
-% Direction for maximum unipolar contrast LMS step
-%   Field size: 60.0 deg
-%   Pupil diameter: 8 mm
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxLMS';
-params.primaryHeadRoom = 0.01;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 60.0;
-params.pupilDiameterMm = 8.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
-params.whichReceptorsToIsolate = [1 2 3];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'LMSDirected_600_80_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
 
-%% MaxMelLMS_unipolar_275_60_667
-% Direction for maximum unipolar contrast melanopsin step
-%   Field size: 27.5 deg
-%   Pupil diameter: 6 mm -- for use with 6 mm artificial pupil as part of
-%   pupillometry
-%   bipolar contrast: 66.7%
-%
-% Bipolar contrast is specified to generate, but the result is a 400% unipolar
-% contrast step up relative to the background.
-params = OLDirectionParams_Unipolar;
-params.baseName = 'MaxMelLMS';
-params.primaryHeadRoom = 0.0;
-params.baseModulationContrast = 2/3;
-params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 6.0;
-params.photoreceptorClasses = {'LConeTabulatedAbsorbance','MConeTabulatedAbsorbance','SConeTabulatedAbsorbance','Melanopsin'};
-params.modulationContrast = [params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast params.baseModulationContrast];
-params.whichReceptorsToIsolate = [1, 2, 3, 4];
-params.whichReceptorsToIgnore = [];
-params.whichReceptorsToMinimize = [];
-params.backgroundName = 'MelanopsinDirected_275_60_667';
-params.name = OLDirectionNameFromParams(params);
-if OLDirectionParamsValidate(params)
-    % All validations OK. Add entry to the dictionary.
-    dictionary(params.name) = params;
-end
+
+
 
 %% LightFlux_UnipolarBase
 %
