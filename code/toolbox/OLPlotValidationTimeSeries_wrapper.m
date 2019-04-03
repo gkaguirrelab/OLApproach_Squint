5b74a3a930f521be09f6ea884840c34d30e91b36
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-03 16:32:53 -0400

    hopefully added some local functions

--- code/toolbox/OLPlotValidationTimeSeries_wrapper.m
+++ code/toolbox/OLPlotValidationTimeSeries_wrapper.m
@@ -248,3 +248,126 @@ OLPlotValidationTimeSeries(...
 'excludedSubjectNames', excludedSubjectNames, ...
 'calibrations', calibrations, 'yLim', [26 35], 'saveDir', saveDir, 'firstDateToPlot', firstDateToPlot, 'excludedSessions', excludedSessions  ...
 );
+
+%% Chromaticity plots
+[ sessionList ] = getSessionsList;
+plotFig = figure;
+[ chromaticityAccumulator ] = summarizeChromaticity(sessionList);
+print(plotFig, fullfile(saveDir, 'chromaticity.pdf'), '-dpdf', '-fillpage');
+
+%% Local functions
+function [ sessionList ] = getSessionsList(varargin)
+
+p = inputParser; p.KeepUnmatched = true;
+p.addParameter('whichDirectory','DataFiles',@ischar);
+p.addParameter('makePlots',false,@islogical);
+% Parse and check the parameters
+p.parse(varargin{:});
+
+projectName = 'melSquintAnalysis';
+directionObjectsBase = fullfile(getpref(projectName, 'melaDataPath'), 'Experiments/OLApproach_Squint/SquintToPulse/', p.Results.whichDirectory);
+
+% determine the session for this subject
+sessions = dir(fullfile(directionObjectsBase, 'MELA*'));
+
+sessionList.ID = [];
+sessionList.date = [];
+counter = 1;
+for subjectIndex = 1:size(sessions,1)
+    potentialSessionDates = dir(fullfile(directionObjectsBase, sessions(subjectIndex).name));
+    for potentialDateIndex = 1:size(potentialSessionDates,1)
+        if strcmp(potentialSessionDates(potentialDateIndex).name(1), '2')
+            % it's a real date
+            sessionList.ID{counter} = sessions(subjectIndex).name;
+            sessionList.date{counter} = potentialSessionDates(potentialDateIndex).name;
+            counter = counter + 1;
+        end
+    end
+end
+
+if p.Results.makePlots
+    for dd = 1:length(sessionList.date)
+        date = strsplit(sessionList.date{dd}, '_');
+        date = date{1};
+        dates(dd) = datenum(date, 'yyyy-mm-dd');
+        sortedDates = sort(dates);
+    end
+    plotFig = figure;
+    plot(sortedDates, 1:length(sortedDates))
+    axesInfo = gca;
+    x1OG = axesInfo.XLim(1);
+    x2OG =  axesInfo.XLim(2);
+    datetick('x', 29)
+    xlabel('Date')
+    ylabel('Total Number of Sessions')
+    
+    plotFig = figure;
+    hold on
+    
+    
+    plot(sortedDates, 1:length(sortedDates))
+    axesInfo = gca;
+    x1 = axesInfo.XLim(1);
+    x2 = axesInfo.XLim(2);
+    line([x1OG, x2OG], [480 480], 'Color', 'r', 'LineStyle', '--')
+    datetick('x', 29)
+    xlabel('Date')
+    ylabel('Total Number of Sessions')
+    ylim([0 500])
+    xlim([x1, x2])
+    
+    plotFig = figure;
+    hold on
+    plot(sortedDates, 1:length(sortedDates))
+    x = sortedDates(53:end);
+    y = 53:length(sortedDates);
+    c = polyfit(x,y,1);
+    y_est = polyval(c,737331:(datenum('2020-01-01', 'yyyy-mm-dd')));
+    x_new = 737331:(datenum('2020-01-01', 'yyyy-mm-dd'));
+    plot(x_new, y_est, 'Color', 'b', 'LineStyle', '--')
+    axesInfo = gca;
+    x1 = axesInfo.XLim(1);
+    x2 = axesInfo.XLim(2);
+    line([x1, x2], [480 480], 'Color', 'r', 'LineStyle', '--')
+    xlabel('Date')
+    ylabel('Total Number of Sessions')
+    ylim([0 500]);
+    xlim([x1OG, x_new(410)]);
+    datetick('x', 29, 'keeplimits')
+
+end
+
+
+end
+
+function [ chromaticityAccumulator ] = summarizeChromaticity(sessionList)
+
+projectName = 'melSquintAnalysis';
+directionObjectsBase = fullfile(getpref(projectName, 'melaDataPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DirectionObjects');
+
+
+for ss = 1:length(sessionList.ID)
+    load(fullfile(directionObjectsBase, sessionList.ID{ss}, sessionList.date{ss}, 'MaxMelDirection.mat'))
+    [ XYChromaticity ] = calculateChromaticity(MaxMelDirection, 'whichValidation', 'postcorrection');
+    chromaticityAccumulator.Mel(ss,1) = XYChromaticity(1);
+    chromaticityAccumulator.Mel(ss,2) = XYChromaticity(2);
+    
+    load(fullfile(directionObjectsBase, sessionList.ID{ss}, sessionList.date{ss}, 'MaxLMSDirection.mat'))
+    [ XYChromaticity ] = calculateChromaticity(MaxLMSDirection, 'whichValidation', 'postcorrection');
+    chromaticityAccumulator.LMS(ss,1) = XYChromaticity(1);
+    chromaticityAccumulator.LMS(ss,2) = XYChromaticity(2);
+    
+    load(fullfile(directionObjectsBase, sessionList.ID{ss}, sessionList.date{ss}, 'LightFluxDirection.mat'))
+    [ XYChromaticity ] = calculateChromaticity(LightFluxDirection, 'whichValidation', 'postcorrection');
+    chromaticityAccumulator.LightFlux(ss,1) = XYChromaticity(1);
+    chromaticityAccumulator.LightFlux(ss,2) = XYChromaticity(2);
+end
+
+%figure;
+%hold on
+plot(chromaticityAccumulator.LightFlux(:,1), chromaticityAccumulator.LightFlux(:,2), 'o', 'Color', 'y')
+plot(chromaticityAccumulator.Mel(:,1), chromaticityAccumulator.Mel(:,2), 'o', 'Color', 'c')
+plot(chromaticityAccumulator.LMS(:,1), chromaticityAccumulator.LMS(:,2), 'o', 'Color', 'k')
+
+
+end
