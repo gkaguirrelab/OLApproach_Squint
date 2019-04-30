0b9af2f9a88a9fc1b436781fa5d6326587cd73ef
melanopsin <melanopsin@gka06.psych.upenn.edu>
2019-04-30 15:57:58 -0400

    added new calibration date

--- code/toolbox/OLPlotValidationTimeSeries_wrapper.m
+++ code/toolbox/OLPlotValidationTimeSeries_wrapper.m
@@ -57,12 +57,12 @@ excludedSessions.names = {'MELA_0120', 'MELA_0147'};
 excludedSessions.dates = {'2018-08-24_session_3', '2018-08-29_session_3'};
 
 % additional optional key-value pairs for plotting
-calibrations = {'2018-05-29', '2018-07-11', '2018-09-05', '2018-10-10', '2018-11-13', '2018-12-06', '2019-02-15', '2019-02-20', '2019-03-19'}; % when relevant calibrations happened. 2018-05-29 -> when we switched form ND 0.4 to 0.3. 2018-07-11 -> when we switched from ND 0.3 to 0.2.
+calibrations = {'2018-05-29', '2018-07-11', '2018-09-05', '2018-10-10', '2018-11-13', '2018-12-06', '2019-02-15', '2019-02-20', '2019-03-19', '2019-03-29', '2019-04-26'}; % when relevant calibrations happened. 2018-05-29 -> when we switched form ND 0.4 to 0.3. 2018-07-11 -> when we switched from ND 0.3 to 0.2.
 % 2018-09-05: switched to Box A, with ND 0.7 cable
 
 % where we're saving these plots
 saveDir = fullfile(getpref('OLApproach_Squint', 'DataPath'), '../MELA_analysis/Experiments/OLApproach_Squint/SquintToPulse/admin');
-firstDateToPlot = '2019-02-01';
+firstDateToPlot = '2019-02-19';
 %% LMS validations
 OLPlotValidationTimeSeries(...
 'approachName', 'OLApproach_Squint', ...
@@ -251,7 +251,7 @@ OLPlotValidationTimeSeries(...
 
 %% Chromaticity plots
 [ sessionList ] = getSessionsList;
-plotFig = figure;
+plotFig = figure; hold on;
 [ chromaticityAccumulator ] = summarizeChromaticity(sessionList);
 print(plotFig, fullfile(saveDir, 'chromaticity.pdf'), '-dpdf', '-fillpage');
 
